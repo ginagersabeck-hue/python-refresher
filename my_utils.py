7c44ef125ad7b592363c7ad89450feeb232a7d45
Gina Gersbaeck <ginagersabeck@gmail.com>
2026-08-27 20:09:26 +0000

    fix get cols and use it

--- my_utils.py
+++ my_utils.py
@@ -2,8 +2,8 @@ def get_column(file_name, query_column, query_value, result_column):
     results = []
     with open(file_name, "r") as file:
         for line in file:
-            values = line.split()
+            values = line.split(',')
             if values[query_column] == query_value:
-                results.append(values[results_column])
+                results.append(values[result_column])
     
     return results
