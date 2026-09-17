48b1e63d4bdb04b3c72e83a8008a91d8cdc32328
Gina Gersbaeck <ginagersabeck@gmail.com>
2026-09-17 16:45:31 +0000

    4: updated run.sh with 1 succesful run and 2 unsuccessful runs

--- print_fires.py
+++ print_fires.py
@@ -1,13 +1,3 @@
-import my_utils
-
-
-country='United States of America'
-country_column = 0
-fires_column = 3
-file_name = 'Agrofood_co2_emission.csv'
-fires = my_utils.get_column(file_name, country_column, country, result_column=fires_column)
-print(fires)
-
 """Script to extract and print emission values using my_utils.
 """
 
